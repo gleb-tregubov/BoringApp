fc070d28964ad1f3fab41df2a6dd5912ace7a96f
gleb-tregubov <tregubovgleb@gmail.com>
2021-09-24 06:51:04 +0300

    feat: add services network tests

---
+++ BoredAppTests/ServicesTests.swift
@@ -0,0 +1,44 @@
+//
+//  ServicesTests.swift
+//  BoredAppTests
+//
+//  Created by Gleb Tregubov on 24.09.2021.
+//
+
+import XCTest
+import BoredApp
+
+class ServicesTests: XCTestCase {
+    
+    var services: Services!
+
+    override func setUpWithError() throws {
+        super.setUp()
+        self.services = Services()
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+
+    
+    func testServices() throws {
+        let expecation = XCTestExpectation(description: "servicesExpectation")
+        
+        self.services.loadActivity { activity, error in
+            
+            if let _ = error {
+                XCTAssertNotNil(error)
+                XCTAssertNil(activity)
+            } else {
+                XCTAssertNotNil(activity)
+                XCTAssertNil(error)
+            }
+
+            expecation.fulfill()
+        }
+        
+        wait(for: [expecation], timeout: 1.0)
+    }
+
+}
